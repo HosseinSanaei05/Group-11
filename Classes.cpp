ccab850db40d8ae4b96df455aafb1dcfa018b93c
HosseinSanaei05 <hosseinmehrad84@gmail.com>
2025-04-09 23:46:28 +0330

    creating the file of classes

---
+++ Classes.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class column
+{
+private:
+string columnName;
+string dataType ;
+
+void fixing_dataType(string type)
+{
+    
+    
+}
+};
+
+class schema
+{
+    private:
+    string tableName;
+    public:
+schema (string name ):tableName(name){};
+
+
+ 
+
+
+};
+
+
+
+
+int main()
+{
+    
+
+    return 0;
+}
